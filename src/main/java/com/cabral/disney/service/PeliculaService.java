fcf75fc29c399eccf95d32e5b8a17e4a62ff6891
Ignacio Agustin Cabral <ignaciocabral97@gmail.com>
2023-08-21 19:16:43 -0300

    test: added PeliculaService test, get pelicula by id

--- src/main/java/com/cabral/disney/service/PeliculaService.java
+++ src/main/java/com/cabral/disney/service/PeliculaService.java
@@ -1,6 +1,7 @@
 package com.cabral.disney.service;
 
 import com.cabral.disney.dto.PeliculaDTO;
+import com.cabral.disney.exception.PeliculaNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,4 +9,6 @@ import java.util.List;
 @Service
 public interface PeliculaService {
     List<PeliculaDTO> getAllPeliculas();
+
+    PeliculaDTO getPeliculaById(Long id) throws PeliculaNotFoundException;
 }
