fcf8e60216197ab926a21687255f8280d0919cfb
Ignacio Agustin Cabral <ignaciocabral97@gmail.com>
2023-08-21 00:43:34 -0300

    test: added PersonajeService test ,searching a Personaje by name should return a non-empty List of PersonajeDTOs

---
+++ src/main/java/com/cabral/disney/exception/PersonajeSearchResultEmptyException.java
@@ -0,0 +1,7 @@
+package com.cabral.disney.exception;
+
+public class PersonajeSearchResultEmptyException extends Exception {
+    public PersonajeSearchResultEmptyException(String message) {
+        super(message);
+    }
+}
