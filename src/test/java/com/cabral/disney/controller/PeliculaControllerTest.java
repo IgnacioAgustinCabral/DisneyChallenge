35d7a961bd8e5bc602e3070b4eacc95d5826f680
Ignacio Agustin Cabral <ignaciocabral97@gmail.com>
2023-08-23 20:29:10 -0300

    test: added test to PeliculaController endpoint POST /personajes/personaje and is returning status code 201 CREATED

--- src/test/java/com/cabral/disney/controller/PeliculaControllerTest.java
+++ src/test/java/com/cabral/disney/controller/PeliculaControllerTest.java
@@ -1,29 +1,42 @@
 package com.cabral.disney.controller;
 
+import com.cabral.disney.dto.PeliculaDTO;
 import com.cabral.disney.exception.PeliculaNotFoundException;
 import com.cabral.disney.service.PeliculaService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.time.LocalDate;
+
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(PeliculaController.class)
+@WebMvcTest(controllers = PeliculaController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
 @AutoConfigureMockMvc(addFilters = false)
 @ExtendWith(MockitoExtension.class)
 public class PeliculaControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @MockBean
     private PeliculaService peliculaService;
@@ -33,7 +46,7 @@ public class PeliculaControllerTest {
     public void testGetAllPeliculasEndpointAndResponseIs200_OK() throws Exception {
         ResultActions response = mockMvc.perform(get("/peliculas/pelicula/all"));
 
-        response.andExpect(MockMvcResultMatchers.status().isOk());
+        response.andExpect(status().isOk());
     }
 
     @Test
@@ -41,7 +54,7 @@ public class PeliculaControllerTest {
 
         ResultActions response = mockMvc.perform(get("/peliculas/pelicula/{id}", 1));
 
-        response.andExpect(MockMvcResultMatchers.status().isOk());
+        response.andExpect(status().isOk());
     }
 
     @Test
@@ -50,6 +63,21 @@ public class PeliculaControllerTest {
 
         ResultActions response = mockMvc.perform(get("/peliculas/pelicula/{id}", 1));
 
-        response.andExpect(MockMvcResultMatchers.status().isNotFound());
+        response.andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testCreatePeliculaEndpointAndResponseIs201_CREATED() throws Exception {
+        LocalDate fechaCreacion = LocalDate.of(2023, 8, 12);
+        PeliculaDTO peliculaDTO = new PeliculaDTO(1L, "asd", fechaCreacion, 1, "imagen");
+
+        ResultActions response = mockMvc.perform(post("/peliculas/pelicula")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(peliculaDTO))
+                .characterEncoding("utf-8"));
+
+        response.andExpect(status().isCreated());
+//                response.andExpect(jsonPath("$.titulo").value("asd"));
+
     }
 }
