ecdae05fd4edbe603c0ee3155c283374be2c5506
Ignacio Agustin Cabral <ignaciocabral97@gmail.com>
2023-08-23 20:29:10 -0300

    test: added test to PeliculaController endpoint POST /personajes/personaje and is returning status code 201 CREATED

--- src/main/java/com/cabral/disney/controller/PeliculaController.java
+++ src/main/java/com/cabral/disney/controller/PeliculaController.java
@@ -5,11 +5,9 @@ import com.cabral.disney.exception.PeliculaNotFoundException;
 import com.cabral.disney.service.PeliculaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,7 +15,7 @@ import java.util.List;
 @RequestMapping("/peliculas")
 public class PeliculaController {
 
-    private PeliculaService peliculaService;
+    private final PeliculaService peliculaService;
 
     @Autowired
     public PeliculaController(PeliculaService peliculaService) {
@@ -37,5 +35,12 @@ public class PeliculaController {
         } catch (PeliculaNotFoundException exception) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
         }
+    }
+
+    @PostMapping(value = "/pelicula")
+    @ResponseBody
+    public ResponseEntity<PeliculaDTO> createPelicula(@RequestBody PeliculaDTO peliculaDTO) {
+        PeliculaDTO createdPelicula = this.peliculaService.createPelicula(peliculaDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdPelicula);
     }
 }
