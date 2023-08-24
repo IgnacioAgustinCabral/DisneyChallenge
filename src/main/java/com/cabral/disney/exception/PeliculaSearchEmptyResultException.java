512f934961c3e6b1657d0258c7457406c970d37b
Ignacio Agustin Cabral <ignaciocabral97@gmail.com>
2023-08-24 19:36:09 -0300

    test: added test to PeliculaController search endpoint GET /personajes/personaje/?name= and is returning status code 404 NOT FOUND

--- src/main/java/com/cabral/disney/exception/PeliculaSearchEmptyResultException.java
+++ src/main/java/com/cabral/disney/exception/PeliculaSearchEmptyResultException.java
@@ -1,6 +1,6 @@
 package com.cabral.disney.exception;
 
-public class PeliculaSearchEmptyResultException extends Throwable {
+public class PeliculaSearchEmptyResultException extends Exception {
     private static final long serialVersionUID = 4L;
 
     public PeliculaSearchEmptyResultException(String message) {
