ff424252e05e13fe001a7105aef8e727d72e25ed
Ignacio Agustin Cabral <ignaciocabral97@gmail.com>
2023-09-06 19:29:14 -0300

    feat: added User and Role repositories

---
+++ src/main/java/com/cabral/disney/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.cabral.disney.repository;
+
+import com.cabral.disney.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername (String username);
+}
