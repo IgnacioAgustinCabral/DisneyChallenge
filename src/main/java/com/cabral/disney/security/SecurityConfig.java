ec6b8b54751406f4e5eec1ddacefe514c3df4879
Ignacio Agustin Cabral <ignaciocabral97@gmail.com>
2023-09-07 00:30:18 -0300

    feat: added role based authentication

--- src/main/java/com/cabral/disney/security/SecurityConfig.java
+++ src/main/java/com/cabral/disney/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,7 +28,7 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeRequests(auth -> auth
-                        .mvcMatchers("/personajes/**").authenticated()
+                        .mvcMatchers("/personajes/**").hasRole("ADMIN")
                         .anyRequest().permitAll())
                 .userDetailsService(myUserDetailsService)
                 .httpBasic(withDefaults())
