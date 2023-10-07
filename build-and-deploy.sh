58eb57ed68d111f747c04037b43895612ea6061c
Ignacio Agustin Cabral <ignaciocabral97@gmail.com>
2023-10-07 15:32:30 -0300

    feat: Added script to build and deploy to docker

---
+++ build-and-deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Run Maven clean and package
+echo "Building the application with Maven..."
+mvn clean package
+
+# Check if the Maven build was successful
+if [ $? -eq 0 ]; then
+  # Start the application using Docker Compose
+  echo "Starting the application with Docker Compose..."
+  docker-compose up
+else
+  echo "Maven build failed. Aborting Docker Compose."
+fi
