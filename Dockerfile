f3f455f165a460e4cf6c319a2011f2d733f23d25
Ignacio Agustin Cabral <ignaciocabral97@gmail.com>
2023-11-04 13:58:28 -0300

    refactor: Restructured Dockerfile for multi-stage build

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,19 @@
-FROM maven:3.9.1-eclipse-temurin-11-alpine
+FROM maven:3.9.1-eclipse-temurin-11-alpine AS build
+
+WORKDIR /app
+
+COPY pom.xml .
+
+RUN mvn -e -B dependency:resolve
+
+COPY src ./src
+
+RUN mvn -e -B clean package -DskipTests
+
+FROM eclipse-temurin:11-alpine
 
 EXPOSE 8080
 
-COPY target/disney-0.0.1-SNAPSHOT.jar  app.jar
+COPY --from=build /app/target/disney-0.0.1-SNAPSHOT.jar app.jar
 
 ENTRYPOINT ["java","-jar","app.jar"]
--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-echo "Cleaning up dangling images..."
 docker image prune -f
 
-# Run Maven clean and package
-echo "Building the application with Maven..."
-mvn clean package -DskipTests
-
-# Check if the Maven build was successful
-if [ $? -eq 0 ]; then
-  # Start the application using Docker Compose
-  echo "Starting the application with Docker Compose..."
-  docker-compose build --no-cache
-  docker-compose up
-else
-  echo "Maven build failed. Aborting Docker Compose."
-fi
+# Start the application using Docker Compose
+echo "Starting the application with Docker Compose..."
+docker-compose build --no-cache
+docker-compose up
