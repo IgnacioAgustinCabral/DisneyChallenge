f3acab6ec989ae1b5dc928e5e50c415d6a0d55c5
Ignacio Agustin Cabral <ignaciocabral97@gmail.com>
2023-12-11 00:44:05 -0300

    test: Added Repository test for adding movies to user watchlist

---
+++ src/main/java/com/cabral/disney/repository/WatchlistRepository.java
@@ -0,0 +1,11 @@
+package com.cabral.disney.repository;
+
+import com.cabral.disney.models.Watchlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface WatchlistRepository extends JpaRepository<Watchlist, Long> {
+    List<Watchlist> findAllByUser_Id(Long id);
+}
